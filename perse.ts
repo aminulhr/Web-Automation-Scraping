b0e8950dd620bdbfda40a8875781b34bba3379dd
aminulhr <aminulhaquerana1@gmail.com>
2025-06-25 23:13:34 +0600

    parse data

--- perse.ts
+++ perse.ts
@@ -1,7 +1,19 @@
 import fs from "fs";
 import { JSDOM } from "jsdom";
-const data = await fs.promises.readFile("example.curl.html", "utf-8");
-// console.log(data);
+const exists = fs.promises
+  .access("example.curl.html", fs.constants.F_ok)
+  .then(() => true)
+  .catch(() => false);
+let data;
+if (exists) {
+  data = fs.promises.writeFile("example.curl.html", "utf-8");
+} else {
+  data = await fetch("https://www.example.com/").then((response) =>
+    response.text()
+  );
+  fs.promises.writeFile("example.puppeteer.html", data);
+}
+
 const dom = new JSDOM(data);
 const document = dom.window.document;
 console.log(document.querySelector("a").href);
