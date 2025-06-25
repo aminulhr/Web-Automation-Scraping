930d9c0bba45a76dd4d50c09952b659cc24b9bc8
aminulhr <aminulhaquerana1@gmail.com>
2025-06-25 22:00:17 +0600

    update

--- perse.ts
+++ perse.ts
@@ -1,8 +1,7 @@
 import fs from "fs";
 import { JSDOM } from "jsdom";
 const data = await fs.promises.readFile("example.curl.html", "utf-8");
-console.log(data);
-
+// console.log(data);
 const dom = new JSDOM(data);
 const document = dom.window.document;
 console.log(document.querySelector("a").href);
