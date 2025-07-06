6bf0630aa362660edbe2258f00553b2bfbdc0196
aminulhr <aminulhaquerana1@gmail.com>
2025-07-06 14:54:58 +0600

    Wait for the full page to load after clicking the link

--- key.ts
+++ key.ts
@@ -7,12 +7,10 @@ async function main() {
 
   const page = await browser.newPage();
 
-  await page.goto("https://excalidraw.com/");
-  await page.keyboard.press("7");
-  await page.mouse.move(400, 300, { steps: 10 });
-  await page.mouse.down();
-  await page.mouse.move(400, 600, { steps: 10 });
-  await page.mouse.up();
+  await page.goto("https://example.com/");
+  await page.click("a");
+  await page.waitForNetworkIdle(); // Wait for the full page to load after clicking the link
+  await page.screenshot({ path: "example link.png" });
   await browser.close();
 }
 main().catch(console.error);
